c7692bcb5a95617affbcaf22e283b3c36c517f66
yuyakondo <yuyakondo333@gmail.com>
2025-01-25 00:33:00 +0900

    admin/urls.pyでURLを設計

--- admin/urls.py
+++ admin/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
-app_name = 'products'
+app_name = 'admin'
+
 urlpatterns = [
-    path('', views.ProductIndexView.as_view(), name='index'),
-    path('<int:pk>/',views.ProductDetailView.as_view(),name='detail'),
+    # 管理画面の商品に関するURL
+    path('products/', views.AdminProductIndexView.as_view(), name='product_index'),
+    path('products/<int:pk>/',views.AdminProductDetailView.as_view(),name='product_detail'),
+    path('products/<int:pk>/edit',views.AdminProductEditView.as_view(),name='product_edit'),
+    path('products/<int:pk>/update',views.AdminProductUpdateView.as_view(),name='product_update'),
+    path('products/<int:pk>/delete',views.AdminProductDeleteView.as_view(),name='product_delete'),
+
+    # 管理画面のログイン、ログアウトに関するURL
+    path('login',views.AdminLoginView.as_view(),name='login'),
+    path('logout',views.AdminLogoutView.as_view(),name='logout'),
 ]
