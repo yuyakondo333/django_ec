7ad3e5a33c0ccbdca34a0a09b7c1a87960a4e642
yuyakondo <yuyakondo333@gmail.com>
2025-01-24 23:26:59 +0900

    admin/urls.pyを作成

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('products/', include('products.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
