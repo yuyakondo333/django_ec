97e3a7586abf33e0c2b876894bc564155972732e
yuyakondo <yuyakondo333@gmail.com>
2025-01-25 00:51:56 +0900

    admin/urls.pyを修正, views.pyでそれぞれのclassだけ作成

--- admin/urls.py
+++ admin/urls.py
@@ -4,14 +4,14 @@ from . import views
 app_name = 'admin'
 
 urlpatterns = [
-    # 管理画面の商品に関するURL
-    path('products/', views.AdminProductIndexView.as_view(), name='product_index'),
-    path('products/<int:pk>/',views.AdminProductDetailView.as_view(),name='product_detail'),
-    path('products/<int:pk>/edit',views.AdminProductEditView.as_view(),name='product_edit'),
-    path('products/<int:pk>/update',views.AdminProductUpdateView.as_view(),name='product_update'),
-    path('products/<int:pk>/delete',views.AdminProductDeleteView.as_view(),name='product_delete'),
-
     # 管理画面のログイン、ログアウトに関するURL
     path('login',views.AdminLoginView.as_view(),name='login'),
     path('logout',views.AdminLogoutView.as_view(),name='logout'),
+    
+    # 管理画面の商品に関するURL
+    path('products/', views.AdminProductIndexView.as_view(), name='product_index'),
+    path('products/create', views.AdminProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/',views.AdminProductDetailView.as_view(),name='product_detail'),
+    path('products/<int:pk>/update',views.AdminProductUpdateView.as_view(),name='product_update'),
+    path('products/<int:pk>/delete',views.AdminProductDeleteView.as_view(),name='product_delete'),
 ]
--- admin/views.py
+++ admin/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
+
+class AdminLoginView():
+    pass
+
+
+class AdminLogoutView():
+    pass
+
+
+class AdminProductIndexView(ListView):
+    pass
+
+
+class AdminProductCreateView(CreateView):
+    pass
+
+
+class AdminProductDetailView(DetailView):
+    pass
+
+
+class AdminProductUpdateView(UpdateView):
+    pass
+
+
+class AdminProductDeleteView(DeleteView):
+    pass
