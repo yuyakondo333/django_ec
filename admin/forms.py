fcc4f8678672b974414675155560280d7873c150
yuyakondo <yuyakondo333@gmail.com>
2025-01-25 14:01:21 +0900

    ログイン エラーメッセージ追加

--- admin/forms.py
+++ admin/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
 
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
@@ -8,3 +9,15 @@ class LoginForm(AuthenticationForm):
             field.widget.attrs['class'] = 'form-control'
             # 全てのフォームの部品にpaceholderを定義して、入力フォームにフォーム名が表示されるように指定。
             field.widget.attrs['placeholder'] = field.label
+
+    # username, password両方合わせたバリデーションチェック
+    def clean(self):
+        clean_data = super().clean()
+        username = clean_data.get('username')
+        password = clean_data.get('password')
+
+        # ユーザー名またはパスワードが空の場合のバリデーション
+        if not username or not password:
+            raise ValidationError("ユーザー名とパスワードを入力してください")
+        
+        return clean_data
