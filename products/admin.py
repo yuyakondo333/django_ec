a318baf45941d035c50a0c0e889fe8355f61526b
yuyakondo <yuyakondo333@gmail.com>
2025-01-27 02:41:53 +0900

    products/admin.pyの内容を削除(custom_admin使用の為)

--- products/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Product
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "price")
-
-admin.site.register(Product, ProductAdmin)
