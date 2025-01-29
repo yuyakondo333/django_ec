3fe1e21eca6c84aec85215e402242aa48703405a
yuyakondo <yuyakondo333@gmail.com>
2025-01-29 23:29:56 +0900

    一覧、詳細画面からカートページへ遷移

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class CartPageView(TemplateView):
+    template_name = 'cart/cart_page.html'
--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('products/', include('products.urls')),
+    path('cart/', include('cart.urls')),
     path('admin/', include('custom_admin.urls', namespace='custom_admin')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
