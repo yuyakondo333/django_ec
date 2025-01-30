5d8ef904ec541b341e87c86ac66aadfe5eeddc4f
yuyakondo <yuyakondo333@gmail.com>
2025-01-31 00:00:53 +0900

    詳細画面の個数選択のバリデーション実装

--- cart/views.py
+++ cart/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views.generic import TemplateView
 from .models import Cart, CartProduct
 from products.models import Product
+from .forms import AddToCartForm
 
 # Create your views here.
 class CartPageView(TemplateView):
@@ -12,6 +13,12 @@ class CartPageView(TemplateView):
 
 class AddToCartView(TemplateView):
     def post(self, request, product_id):
+        form = AddToCartForm(request.POST)
+        if not form.is_valid():
+            messages.error(request, form.errors["num"][0])  # エラーメッセージを表示
+            return redirect(request.META.get("HTTP_REFERER", reverse("products:index")))
+
+        num = form.cleaned_data["num"]  # ✅ バリデーション済みの値を取得
         # セッションIDを取得（なければ作成）
         session_id = request.session.session_key
         if not session_id:
@@ -24,10 +31,25 @@ class AddToCartView(TemplateView):
         # 商品を取得
         product = get_object_or_404(Product, id=product_id)
 
+        """
+        詳細画面から個数指定された際に
+        request.POST.get()でname="num"となっているinputタグから取得
+        「Add to cart」をクリックした際はデフォルトの1を使用
+        フォームのデータ型は文字列。整数を扱いたいからint型に変換
+        """
+        num = request.POST.get("num", "1")
+        num = int(num)
+        
         # カート内の商品を取得または作成
+        # createdは「今回新しく作成された」かどうか（boolean)
         cart_product, created = CartProduct.objects.get_or_create(cart=cart, product=product)
+        
+        # 今回新しく作成されていない→→既にレコードがある場合
         if not created:
-            cart_product.quantity += 1
+            cart_product.quantity += num
+        else:
+            # 今回 Add to cartでcart_productテーブルに新しいレコードを追加する場合
+            cart_product.quantity = num
         cart_product.save()
 
         # 合計個数
