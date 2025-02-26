301c6199283e14df22f8aa6c59770ac3333f1235
yuyakondo <yuyakondo333@gmail.com>
2025-02-27 02:25:08 +0900

    カスタムコマンド DB保存完了

--- promotion_code/management/commands/promotion_code_generate.py
+++ promotion_code/management/commands/promotion_code_generate.py
@@ -1,7 +1,52 @@
+import random, string
 from django.core.management.base import BaseCommand
+from promotion_code.models import PromotionCode
+from django.db import IntegrityError, OperationalError
+
+# 英数字7桁でランダムにプロモーションコードを10個生成
+def promotion_code_generate():
+    CODE_LENGTH = 7
+    NUM_CODES = 10
+
+    chars = string.ascii_letters + string.digits  
+    promotion_code_list = [''.join(random.choices(chars, k=CODE_LENGTH)) for _ in range(NUM_CODES)]
+    return promotion_code_list
+
+# 各プロモーションコードに割引額を当てはめる ¥100~¥1,000
+def generate_discount(promotion_code_list):
+    discount_list = list(range(100, 1001, 100))
+    discount_dict = {}
+
+    for promotion_code in promotion_code_list:
+        discount_dict[promotion_code] = random.choice(discount_list)
+    return discount_dict
 
 class Command(BaseCommand):
-    help = "Help message"
+    help = "custom command : promotion_code_generate"
 
     def handle(self, *args, **options):
-        print("Hello, World!")
+        try:
+            promotion_codes = promotion_code_generate()
+            discount_mapping = generate_discount(promotion_codes)
+
+            # 作成した{プロモーションコード:割引額}をDBに保存
+            for code, discount in discount_mapping.items():
+                try:
+                    PromotionCode.objects.create(
+                        promo_code=code,
+                        discount=discount
+                    )
+                except IntegrityError:
+                    error_message = f"プロモーションコード'{code}'が重複しているため、保存できませんでした"
+                    self.stderr.write(self.style.ERROR(error_message))
+
+            # コンソールにサクセスメッセージを出力
+            self.stdout.write(self.style.SUCCESS("Successfully generated 10 promotion codes!"))
+
+        except OperationalError:
+            error_message = "データベースに接続できませんでした"
+            self.stderr.write(self.style.ERROR(error_message))
+
+        except Exception as e:
+            error_msg = f"不明なエラーが発生しました: {str(e)}"
+            self.stderr.write(self.style.ERROR(error_message))
--- promotion_code/models.py
+++ promotion_code/models.py
@@ -1,3 +1,16 @@
+import random, string
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class PromotionCode(models.Model):
+    promo_code = models.CharField("プロモーションコード", max_length=7, unique=True)
+    discount = models.IntegerField("割引額")
+    is_used = models.BooleanField("使用済み判定", default=False)
+    used_at = models.DateTimeField("使用日時", default=timezone.now)
+
+    class Meta:
+        db_table = 'promotion_code'
+
+    def __str__(self):
+        return f"{self.promo_code} : {self.discount}"
